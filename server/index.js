3f0ac6c6c0dec65100921871ef25bd86446cdee9
Kealen-Pillay <kealenpillay@gmail.com>
2022-11-11 11:40:59 +1300

    Connecting backend to database

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 var cors = require("cors");
 const connectDB = require("./config/db");
-
-const items = require("../routes/api/items");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
-const port = process.env.PORT || 8082;
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
-app.use("/api/items", items);
+
+const CONNECTION_URL =
+  "mongodb+srv://test:test@to-do-app.ygzzapa.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 8082;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
