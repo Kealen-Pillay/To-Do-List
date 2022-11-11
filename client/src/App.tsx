9d59b30bda014209eacaca225448108b9885b3c4
Kealen-Pillay <kealenpillay@gmail.com>
2022-11-11 16:35:16 +1300

    Added React Router routes between login and task screens

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginScreen from "./components/LoginScreen";
+import TaskScreen from "./components/TaskScreen";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginScreen />} />
+          <Route path="/tasks" element={<TaskScreen />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
