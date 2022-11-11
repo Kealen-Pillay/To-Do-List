917edb21769f438277dddd135c4572d4a9b3254f
Kealen-Pillay <kealenpillay@gmail.com>
2022-11-11 16:49:01 +1300

    Created transparent card on Login Screen

--- client/src/components/LoginScreen.tsx
+++ client/src/components/LoginScreen.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
 
 const LoginScreen = () => {
-  return <div>LoginScreen</div>;
+  return (
+    <Box
+      sx={{
+        background: "linear-gradient(to right, #333999, #ff00cc)",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        sx={{
+          width: "50%",
+          height: "70%",
+          backgroundColor: "white",
+          opacity: 0.5,
+          borderRadius: 5,
+        }}
+      ></Box>
+    </Box>
+  );
 };
 
 export default LoginScreen;
