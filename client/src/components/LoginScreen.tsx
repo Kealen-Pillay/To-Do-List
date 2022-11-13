fde43d4e9fbe3a7d419f509598eef516d51759ac
Kealen-Pillay <kealenpillay@gmail.com>
2022-11-13 15:23:59 +1300

    Added login and register buttons

--- client/src/components/LoginScreen.tsx
+++ client/src/components/LoginScreen.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import { Box, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 const LoginScreen = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <Box
       sx={{
-        background: "linear-gradient(to right, #333999, #ff00cc)",
+        background: "linear-gradient(to right, #ff875c, #eb346b)",
         width: "100vw",
         height: "100vh",
         display: "flex",
@@ -15,7 +18,7 @@ const LoginScreen = () => {
     >
       <Box
         sx={{
-          width: "50%",
+          width: "60%",
           height: "70%",
           backgroundColor: "white",
           opacity: 0.5,
@@ -38,7 +41,7 @@ const LoginScreen = () => {
           }}
         >
           <img src={require("../images/tickIcon.png")} alt="" height="100%" />
-          <Typography sx={{ fontWeight: "bold", fontSize: 40 }}>
+          <Typography sx={{ fontWeight: "bold", fontSize: 60 }}>
             To Do
           </Typography>
         </Box>
@@ -52,8 +55,13 @@ const LoginScreen = () => {
             margin: "1%",
           }}
         >
-          <Typography>Username:</Typography>
-          <TextField variant="outlined" />
+          <TextField
+            variant="outlined"
+            label="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            sx={{ width: "100%" }}
+          />
         </Box>
         <Box
           sx={{
@@ -63,10 +71,55 @@ const LoginScreen = () => {
             justifyContent: "space-between",
             alignItems: "center",
             margin: "1%",
+            marginBottom: "5%",
           }}
         >
-          <Typography>Password:</Typography>
-          <TextField variant="outlined" />
+          <TextField
+            variant="outlined"
+            label="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            sx={{ width: "100%" }}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            width: "40%",
+            height: "20%",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "black",
+              width: "100%",
+              height: "45%",
+              ":hover": {
+                backgroundColor: "black",
+                transform: "scale(1.05)",
+              },
+            }}
+          >
+            <Typography>Register</Typography>
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "black",
+              width: "100%",
+              height: "45%",
+              ":hover": {
+                backgroundColor: "black",
+                transform: "scale(1.05)",
+              },
+            }}
+          >
+            <Typography>Login</Typography>
+          </Button>
         </Box>
       </Box>
     </Box>
