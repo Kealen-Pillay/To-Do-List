2c30eb093b14ef8898787086c6e5bf45d36b74e0
Kealen-Pillay <kealenpillay@gmail.com>
2022-11-13 16:05:17 +1300

    Added base styling for register screen

--- client/src/components/RegisterScreen.tsx
+++ client/src/components/RegisterScreen.tsx
@@ -1,7 +1,19 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 const RegisterScreen = () => {
-  return <div>RegisterScreen</div>;
+  return (
+    <Box
+      sx={{
+        background: "linear-gradient(to right, #ff875c, #eb346b)",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    ></Box>
+  );
 };
 
 export default RegisterScreen;
