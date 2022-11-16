ba9afe3dd0afc11a43dc13dbe30240db171c481b
Kealen-Pillay <kealenpillay@gmail.com>
2022-11-16 14:55:05 +1300

    Minor change

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ var cors = require("cors");
 const mongoose = require("mongoose");
 const tasks = require("./routes/items");
 require("dotenv").config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
